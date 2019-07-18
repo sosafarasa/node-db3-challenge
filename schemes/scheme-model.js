c8fa17f580ff84818f326c2ca62541509e35cbec
Agustina Sosa <agustinasf85@gmail.com>
2019-07-17 22:16:15 -0400

    Finished helper methods and fixed typo in findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,7 +4,9 @@ module.exports = {
     find,
     findById,
     findSteps,
-
+    add,
+    update,
+    remove
 }
 
 function find() {
@@ -24,12 +26,43 @@ function findById(id) {
     })
 }
 
-// needs debugging
-
 function findSteps(id) {
-    return db('scheme as s')
+    return db('schemes as s')
     .innerJoin('steps as st', 's.id', 'st.scheme_id')
     .select('st.id', 's.scheme_name', 'st.step_number', 'st.instructions')
-    .where({ scheme_id: id });
+    .where({ scheme_id: id })
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme, 'id')
+    .then(schemes => {
+        const scheme_id = schemes[0];
+        return scheme_id;
+    })
+}
+
+function update(changes, id) {
+    return db('schemes')
+    .where({ id })
+    .update(changes)
+    .then(updated => {
+        if(updated) {
+            return updated;
+        }
+    })
+}
+
+function remove(id) {
+    return db('schemes')
+    .where({ id })
+    .delete()
+    .then(scheme => {
+        if(scheme) {
+            return scheme;
+        } else {
+            return null;
+        }
+    })
 }
 
